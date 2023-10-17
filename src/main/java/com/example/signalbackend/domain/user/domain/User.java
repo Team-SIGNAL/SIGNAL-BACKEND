b5c81b54195452b1b574f1e44db1ef05850b44cf
alsco39 <minchaeg85@gmail.com>
2023-10-17 23:40:48 +0900

    📑 :: UserEntity

---
+++ src/main/java/com/example/signalbackend/domain/user/domain/User.java
@@ -0,0 +1,39 @@
+package com.example.signalbackend.domain.user.domain;
+
+import com.example.signalbackend.global.entity.BaseIdEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tbl_user")
+@Entity
+public class User extends BaseIdEntity {
+
+    @Column(columnDefinition = "VARCHAR(20)", nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private LocalDate birth;
+
+    @Column(columnDefinition = "VARCHAR(11)", nullable = false, unique = true)
+    private String phone;
+
+    @Column(columnDefinition = "VARCHAR(12)", nullable = false, unique = true)
+    private String accountId;
+
+    @Column(columnDefinition = "CHAR(256)", nullable = false)
+    private String password;
+
+    @Column(columnDefinition = "VARCHAR(5)")
+    private String gender;
+}
