c960dab77f16e94922a3b311084259d34b7af155
min56 <minchaeg85@gmail.com>
2023-10-25 21:13:54 +0900

    ⚙️ :: 프로젝트 기초 세팅

---
+++ src/test/java/com/example/signalbackend/SignalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.signalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
