19446895f01090602256ce255bc9ea53bd866dbc
alsco39 <minchaeg85@gmail.com>
2023-10-25 17:29:21 +0900

    ⚙️ :: 프로젝트 제거

--- README.md
+++
@@ -1,3 +0,0 @@
-# SIGNAL-BACKEND
-
-SIGNAL 백엔드 임시서버 레포지토리입니다.
--- src/main/java/com/example/signalbackend/domain/user/domain/repository/RefreshTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.signalbackend.domain.user.domain.repository;
-
-import com.example.signalbackend.domain.refresh.domain.RefreshToken;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
-
-}
