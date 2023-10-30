8372dd228bfc3ab6a8e4aeeae405283b6424b2fc
min56 <minchaeg85@gmail.com>
2023-10-30 20:29:22 +0900

    📑 :: AdminSignupRequest

---
+++ src/main/java/com/example/signalbackend/domain/admin/presentation/dto/request/AdminSignUpRequest.java
@@ -0,0 +1,29 @@
+package com.example.signalbackend.domain.admin.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Getter
+@NoArgsConstructor
+public class AdminSignUpRequest {
+
+    @NotBlank
+    @Size(min = 2, max = 20)
+    private String name;
+
+    @NotBlank
+    @Size(min = 13)
+    private String phone;
+
+    @NotBlank
+    @Size(min = 5, max = 12)
+    private String accountId;
+
+    @NotBlank
+    @Pattern(regexp = "(?=.*[a-z])(?=.*[0-9])(?=.*[!#$%&'()*+,./:;<=>?@＼^_`{|}~])[a-zA-Z0-9!#$%&'()*+,./:;<=>?@＼^_`{|}~]{8,32}$")
+    private String password;
+}
