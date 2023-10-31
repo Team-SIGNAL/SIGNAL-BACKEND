38b70affd8623f52f2cc602651b2381fe1915bde
min56 <minchaeg85@gmail.com>
2023-10-31 14:47:59 +0900

    ♻️ :: import 패키지 변경

--- src/main/java/com/example/signalbackend/global/security/WebConfig.java
+++ src/main/java/com/example/signalbackend/global/security/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.signalbackend.global.config;
+package com.example.signalbackend.global.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
