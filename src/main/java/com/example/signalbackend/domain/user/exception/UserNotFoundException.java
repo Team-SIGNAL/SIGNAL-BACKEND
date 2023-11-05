1900b51443a5a0378fee837f2500ad98ba91cbf4
alsco39 <minchaeg85@gmail.com>
2023-11-05 14:32:21 +0900

    ♻️ :: 공백 삭제

--- src/main/java/com/example/signalbackend/domain/user/exception/UserNotFoundException.java
+++ src/main/java/com/example/signalbackend/domain/user/exception/UserNotFoundException.java
@@ -6,7 +6,6 @@ import com.example.signalbackend.global.error.exception.GlobalException;
 public class UserNotFoundException extends GlobalException {
     public static final GlobalException EXCEPTION = new UserNotFoundException();
 
-
     private UserNotFoundException() {
         super(ErrorCode.USER_NOT_FOUND);
     }
