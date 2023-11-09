a346420d92f50b8ead30b3fe07c8b1c673a36bc3
min56 <minchaeg85@gmail.com>
2023-11-09 16:39:48 +0900

    📑 :: UploadImageResponse

---
+++ src/main/java/com/example/signalbackend/domain/attachment/presentation/dto/response/UploadImageResponse.java
@@ -0,0 +1,10 @@
+package com.example.signalbackend.domain.attachment.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UploadImageResponse {
+    private final String image;
+}
