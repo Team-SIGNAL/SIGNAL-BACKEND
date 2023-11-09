177339df5b7800cb6628e20d43b8bceefc5ca855
min56 <minchaeg85@gmail.com>
2023-11-09 16:56:45 +0900

    ♻️ :: getCurrentAdmin 메소드 추가

--- src/main/java/com/example/signalbackend/domain/admin/facade/AdminFacade.java
+++ src/main/java/com/example/signalbackend/domain/admin/facade/AdminFacade.java
@@ -4,6 +4,7 @@ import com.example.signalbackend.domain.admin.domain.Admin;
 import com.example.signalbackend.domain.admin.domain.repository.AdminRepository;
 import com.example.signalbackend.domain.admin.exception.AdminNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
@@ -14,5 +15,10 @@ public class AdminFacade {
 
     public Admin getAdminByAdminId(String adminId) {
         return adminRepository.findByAdminId(adminId).orElseThrow(() -> AdminNotFoundException.EXCEPTION);
+    }
+
+    public Admin getCurrentAdmin() {
+        String adminId = SecurityContextHolder.getContext().getAuthentication().getName();
+        return getAdminByAdminId(adminId);
     }
 }
