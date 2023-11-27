f9ff97f3b1ae9eb26f8c9084b32c3085fad6f301
min56 <minchaeg85@gmail.com>
2023-11-27 23:38:59 +0900

    ♻️ :: JpaRepository로 변경 및 메소드 추가

--- src/main/java/com/example/signalbackend/domain/admin/domain/repository/AdminRepository.java
+++ src/main/java/com/example/signalbackend/domain/admin/domain/repository/AdminRepository.java
@@ -1,13 +1,20 @@
 package com.example.signalbackend.domain.admin.domain.repository;
 
 import com.example.signalbackend.domain.admin.domain.Admin;
-import org.springframework.data.repository.CrudRepository;
+import com.example.signalbackend.domain.admin.domain.AuthStatus;
+import com.example.signalbackend.domain.admin.domain.Role;
+import com.example.signalbackend.domain.admin.presentation.dto.response.HospitalAuthRequestElement;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface AdminRepository extends CrudRepository<Admin, Long> {
+public interface AdminRepository extends JpaRepository<Admin, UUID> {
     Optional<Admin> findByAdminId(String adminId);
     boolean existsByAdminId(String adminId);
+    List<Admin> findAllByAuthStatusAndRole(AuthStatus authStatus, Role role);
+    List<Admin> findAllByRoleOrderByName(Role role);
 }
