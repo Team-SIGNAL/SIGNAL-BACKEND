d5aec0ab7e4451d72fb7057c239d9513410f7924
alsco39 <minchaeg85@gmail.com>
2023-12-13 13:59:38 +0900

    ♻️ :: Response 추가

--- src/main/java/com/example/signalbackend/domain/admin/presentation/dto/response/HospitalInfoResponse.java
+++ src/main/java/com/example/signalbackend/domain/admin/presentation/dto/response/HospitalInfoResponse.java
@@ -1,5 +1,6 @@
 package com.example.signalbackend.domain.admin.presentation.dto.response;
 
+import com.example.signalbackend.domain.admin.domain.AuthStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,5 +10,7 @@ public class HospitalInfoResponse {
     private final String name;
     private final String phone;
     private final String profile;
-    private final Boolean isNotAuthRequest;
+    private final AuthStatus authStatus;
+    private final String address;
+    private final String hospitalImage;
 }
