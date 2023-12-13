6ad147e85a560520023553d325ddfe67b8757866
alsco39 <minchaeg85@gmail.com>
2023-12-13 14:38:56 +0900

    ♻️ :: findAllByTagOrderByCreateDate 메소드 추가

--- src/main/java/com/example/signalbackend/domain/feed/domain/repository/FeedRepository.java
+++ src/main/java/com/example/signalbackend/domain/feed/domain/repository/FeedRepository.java
@@ -1,7 +1,13 @@
 package com.example.signalbackend.domain.feed.domain.repository;
 
 import com.example.signalbackend.domain.feed.domain.Feed;
+import com.example.signalbackend.domain.feed.domain.Tag;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FeedRepository extends JpaRepository<Feed, Long> {
+import java.util.UUID;
+
+public interface FeedRepository extends JpaRepository<Feed, UUID> {
+    Page<Feed> findAllByTagOrderByCreateDate(Tag tag, Pageable pageable);
 }
