96c22988707da148f1b86fc460847aa5e15626ab
alsco39 <minchaeg85@gmail.com>
2023-12-13 14:41:52 +0900

    ⚡️ :: 피드 조회, 피드 상세보기, 피드 업데이트, 피드 삭제, 피드 신고 API

--- src/main/java/com/example/signalbackend/domain/feed/presentation/FeedController.java
+++ src/main/java/com/example/signalbackend/domain/feed/presentation/FeedController.java
@@ -1,16 +1,27 @@
 package com.example.signalbackend.domain.feed.presentation;
 
+import com.example.signalbackend.domain.feed.domain.Tag;
 import com.example.signalbackend.domain.feed.presentation.dto.request.CreateFeedRequest;
+import com.example.signalbackend.domain.feed.presentation.dto.request.UpdateFeedRequest;
+import com.example.signalbackend.domain.feed.presentation.dto.response.FeedElement;
+import com.example.signalbackend.domain.feed.presentation.dto.response.FeedListResponse;
 import com.example.signalbackend.domain.feed.service.FeedService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @RequestMapping("/feed")
@@ -29,4 +40,46 @@ public class FeedController {
     public void createAdminFeed(@RequestBody @Valid CreateFeedRequest request) {
         feedService.createAdminFeed(request);
     }
+
+    @GetMapping("/user/list")
+    public FeedListResponse queryUserFeedList(@RequestParam(name = "tag")Tag tag, Pageable pageable) {
+        return feedService.queryUserFeedList(tag, pageable);
+    }
+
+    @GetMapping("/admin/list")
+    public FeedListResponse queryAdminFeedList(@RequestParam(name = "tag")Tag tag, Pageable pageable) {
+        return feedService.queryAdminFeedList(tag, pageable);
+    }
+
+    @GetMapping("/user/{feed_id}")
+    public FeedElement queryUserFeedDetails(@PathVariable(name = "feed_id") UUID feedId) {
+        return feedService.queryUserFeedDetails(feedId);
+    }
+
+    @GetMapping("/admin/{feed_id}")
+    public FeedElement queryAdminFeedDetails(@PathVariable(name = "feed_id") UUID feedId) {
+        return feedService.queryAdminFeedDetails(feedId);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping("/{feed_id}")
+    public void updateFeed(
+            @PathVariable(name = "feed_id") UUID feedId,
+            @RequestBody UpdateFeedRequest request
+    ) {
+        feedService.updateFeed(feedId, request);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{feed_id}")
+    public void deleteFeed(@PathVariable(name = "feed_id") UUID feedId) {
+        feedService.deleteFeed(feedId);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping("/report/{feed_id}")
+    public void reportFeed(@PathVariable(name = "feed_id") UUID feedId) {
+        feedService.reportFeed(feedId);
+    }
 }
+
--- src/main/java/com/example/signalbackend/domain/feed/service/FeedService.java
+++ src/main/java/com/example/signalbackend/domain/feed/service/FeedService.java
@@ -3,18 +3,30 @@ package com.example.signalbackend.domain.feed.service;
 import com.example.signalbackend.domain.admin.domain.Admin;
 import com.example.signalbackend.domain.admin.domain.Role;
 import com.example.signalbackend.domain.admin.facade.AdminFacade;
+import com.example.signalbackend.domain.comment.domain.repository.CommentRepository;
 import com.example.signalbackend.domain.feed.domain.Feed;
 import com.example.signalbackend.domain.feed.domain.Tag;
 import com.example.signalbackend.domain.feed.domain.repository.FeedRepository;
 import com.example.signalbackend.domain.feed.exception.CreateFeedNotPermissionException;
+import com.example.signalbackend.domain.feed.exception.FeedNotUpdateException;
+import com.example.signalbackend.domain.feed.facade.FeedFacade;
 import com.example.signalbackend.domain.feed.presentation.dto.request.CreateFeedRequest;
+import com.example.signalbackend.domain.feed.presentation.dto.request.UpdateFeedRequest;
+import com.example.signalbackend.domain.feed.presentation.dto.response.FeedElement;
+import com.example.signalbackend.domain.feed.presentation.dto.response.FeedListElement;
+import com.example.signalbackend.domain.feed.presentation.dto.response.FeedListResponse;
 import com.example.signalbackend.domain.user.domain.User;
 import com.example.signalbackend.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -22,6 +34,8 @@ public class FeedService {
     private final FeedRepository feedRepository;
     private final UserFacade userFacade;
     private final AdminFacade adminFacade;
+    private final FeedFacade feedFacade;
+    private final CommentRepository commentRepository;
 
     @Transactional
     public void createUserFeed(CreateFeedRequest request) {
@@ -33,7 +47,8 @@ public class FeedService {
                 .content(request.getContent())
                 .image(request.getImage())
                 .tag(Tag.GENERAL)
-                        .creatDate(LocalDate.now())
+                .name(user.getName())
+                .createDate(LocalDate.now())
                 .build());
     }
 
@@ -41,10 +56,10 @@ public class FeedService {
     public void createAdminFeed(CreateFeedRequest request) {
         Admin admin = adminFacade.getCurrentAdmin();
 
-        if(admin.getRole() == Role.NON_HOSPITAL) throw CreateFeedNotPermissionException.EXCEPTION;
+        if(admin.getRole().equals(Role.NON_HOSPITAL)) throw CreateFeedNotPermissionException.EXCEPTION;
 
         Tag tag = Tag.HOSPITAL;
-        if(admin.getRole() == Role.ADMIN) tag = Tag.NOTIFICATION;
+        if(admin.getRole().equals(Role.ADMIN)) tag = Tag.NOTIFICATION;
 
         feedRepository.save(Feed.builder()
                 .writerId(admin.getId())
@@ -52,8 +67,136 @@ public class FeedService {
                 .content(request.getContent())
                 .image(request.getImage())
                 .tag(tag)
-                .creatDate(LocalDate.now())
+                .name(admin.getName())
+                .createDate(LocalDate.now())
                 .build());
     }
 
+    @Transactional(readOnly = true)
+    public FeedListResponse queryUserFeedList(Tag tag, Pageable pageable) {
+        List<FeedListElement> feedList = feedRepository.findAllByTagOrderByCreateDate(tag, pageable)
+                .stream()
+                .map(feed -> {
+                    User user = userFacade.getCurrentUser();
+                    boolean checkIsWriter = (user.getId().equals(feed.getWriterId()));
+                    return feedListBuilder(feed, checkIsWriter);
+                })
+                .collect(Collectors.toList());
+
+        long pageTotal = feedList.stream().count();
+
+        return new FeedListResponse(feedList, pageTotal);
+    }
+
+    @Transactional(readOnly = true)
+    public FeedListResponse queryAdminFeedList(Tag tag, Pageable pageable) {
+        List<FeedListElement> feedList = feedRepository.findAllByTagOrderByCreateDate(tag, pageable)
+                .stream()
+                .map(feed -> {
+                    Admin admin = adminFacade.getCurrentAdmin();
+                    boolean checkIsWriter = (admin.getId().equals(feed.getWriterId()));
+                    return feedListBuilder(feed, checkIsWriter);
+                })
+                .collect(Collectors.toList());
+
+        long pageTotal = feedList.stream().count();
+
+        return new FeedListResponse(feedList, pageTotal);
+    }
+
+    private FeedListElement feedListBuilder(Feed feed, Boolean checkIsWriter) {
+        return FeedListElement.builder()
+                .id(feed.getId())
+                .title(feed.getTitle())
+                .image(feed.getImage())
+                .name(feed.getName())
+                .createDate(feed.getCreateDate())
+                .isMine(checkIsWriter).build();
+    }
+
+    @Transactional(readOnly = true)
+    public FeedElement queryUserFeedDetails(UUID feedId) {
+        Feed feed = feedFacade.getFeedById(feedId);
+
+        Optional<User> user = userFacade.getUserById(feed.getWriterId());
+        Optional<Admin> admin = adminFacade.getAdminById(feed.getWriterId());
+        UUID userId = userFacade.getCurrentUser().getId();
+        boolean checkIsWriter = (userId.equals(feed.getWriterId()));
+
+        if(!admin.isPresent()) {
+            return feedDetailsBuilder(feed, user.get().getProfile(), user.get().getName(), checkIsWriter);
+        }
+
+        return feedDetailsBuilder(feed, admin.get().getProfile(), admin.get().getName(), checkIsWriter);
+    }
+
+    @Transactional(readOnly = true)
+    public FeedElement queryAdminFeedDetails(UUID feedId) {
+        Feed feed = feedFacade.getFeedById(feedId);
+
+        Optional<Admin> admin = adminFacade.getAdminById(feed.getWriterId());
+        Optional<User> user = userFacade.getUserById(feed.getWriterId());
+        UUID adminId = adminFacade.getCurrentAdmin().getId();
+        boolean checkIsWriter = (adminId.equals(feed.getWriterId()));
+
+        if(!admin.isPresent()) {
+            return feedDetailsBuilder(feed, user.get().getProfile(), user.get().getName(), checkIsWriter);
+        }
+
+        return feedDetailsBuilder(feed, admin.get().getProfile(), admin.get().getName(), checkIsWriter);
+    }
+
+    private FeedElement feedDetailsBuilder(Feed feed, String profile, String name, Boolean checkIsWriter) {
+        return FeedElement.builder()
+                .id(feed.getId())
+                .image(feed.getImage())
+                .title(feed.getTitle())
+                .createDate(feed.getCreateDate())
+                .writer(feed.getName())
+                .content(feed.getContent())
+                .profile(profile)
+                .isMine(checkIsWriter)
+                .build();
+    }
+
+    @Transactional
+    public void updateFeed(UUID feedId, UpdateFeedRequest request) {
+        Feed feed = feedFacade.getFeedById(feedId);
+
+        if(!checkFeedWriter(feed)) {
+            throw FeedNotUpdateException.EXCEPTION;
+        }
+        feed.updateFeed(request.getTitle(), request.getContent(), request.getImage());
+    }
+
+    @Transactional
+    public void deleteFeed(UUID feedId) {
+        Feed feed = feedFacade.getFeedById(feedId);
+        checkFeedWriter(feed);
+
+        commentRepository.deleteAllByFeed_Id(feedId);
+        feedRepository.deleteById(feed.getId());
+    }
+
+    private Boolean checkFeedWriter(Feed feed) {
+        boolean checkIsWriter = false;
+
+        if(feed.getTag().equals(Tag.GENERAL)) {
+            userFacade.getUserById(feed.getWriterId());
+            UUID userId = userFacade.getCurrentUser().getId();
+            checkIsWriter = (userId.equals(feed.getWriterId()));
+        } else {
+            adminFacade.getAdminById(feed.getWriterId());
+            UUID adminId = adminFacade.getCurrentAdmin().getId();
+            checkIsWriter = (adminId.equals(feed.getWriterId()));
+        }
+
+        return checkIsWriter;
+    }
+
+    @Transactional
+    public void reportFeed(UUID feedId) {
+        Feed feed = feedFacade.getFeedById(feedId);
+        feed.updateReportStatus(true);
+    }
 }
