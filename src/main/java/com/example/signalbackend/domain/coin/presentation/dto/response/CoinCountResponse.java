b9cd2dbcd0540ffe6cad7a1cb7ac9782f2737eb7
alsco39 <minchaeg85@gmail.com>
2023-12-13 14:03:12 +0900

    📑 :: CoinCountResponse

---
+++ src/main/java/com/example/signalbackend/domain/coin/presentation/dto/response/CoinCountResponse.java
@@ -0,0 +1,11 @@
+package com.example.signalbackend.domain.coin.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CoinCountResponse {
+
+    private final Long coinCount;
+}
