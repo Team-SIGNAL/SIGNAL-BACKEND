b13c3d9760deea540565b9aba940dff7c85f97a1
alsco39 <minchaeg85@gmail.com>
2023-12-13 14:53:20 +0900

    ♻️ :: JpaRepository 변경

--- src/main/java/com/example/signalbackend/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/example/signalbackend/domain/user/domain/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.example.signalbackend.domain.user.domain.repository;
 
 import com.example.signalbackend.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByAccountId(String accountId);
     boolean existsByAccountId(String accountId);
 }
